2baccfeb75e950099bcb9768e3b6c335ccbd87b7
GabrielePrestifilippo <gabry.501@hotmail.it>
2017-06-28 16:30:56 +0200

    coordinates from bbox

--- examples/Heatmap.js
+++ examples/Heatmap.js
@@ -2,9 +2,10 @@ var wwd;
 var canvas;
 var heat;
 requirejs(['../src/WorldWind',
-        './LayerManager'],
+        './LayerManager'
+    ],
     function (ww,
-              LayerManager) {
+              LayerManager,) {
         "use strict";
 
         WorldWind.Logger.setLoggingLevel(WorldWind.Logger.LEVEL_WARNING);
@@ -36,7 +37,7 @@ requirejs(['../src/WorldWind',
         heat = simpleheat(canvas);
 
 
-        var points = [[15.0877591, 37.5012762], [15.0877591, 37.5213762], [15.8877591, 37.9213762], [15, 39]];
+        var points = [[15.0877591, 37.5012762], [15.0877591, 37.5213762], [15.8877591, 36]];
         //var points = [[15.0877591, 37.5012762]];
         if (points.length > 1) {
             var maxLng = Math.max.apply(Math, points.map(function (v) {
@@ -73,6 +74,7 @@ requirejs(['../src/WorldWind',
             }
 
             bufferLat = bufferLng = Math.max(bufferLat, bufferLng);
+
             var bbox = {
                 minLat: minLat,
                 minLng: minLng,
@@ -87,7 +89,7 @@ requirejs(['../src/WorldWind',
             points.forEach(function (p) {
                 var out = t.transform(p);
                 var x = out[0];
-                var y = canvas.height - out[1];
+                var y = out[1];
                 heat.add([x, y, 100]);
             });
 
@@ -114,7 +116,7 @@ requirejs(['../src/WorldWind',
 
 
         function drawHeatmap(range) {
-            /*
+
             try {
                 var v = [];
                 var res = wwd.pickTerrain(new WorldWind.Vec3(0, 0, 0)).objects[0].position;
@@ -137,69 +139,99 @@ requirejs(['../src/WorldWind',
                 var minLat = Math.min.apply(Math, v.map(function (v) {
                     return v[1];
                 }));
-
-
+                /*
                 var bufferLng = (maxLng - minLng) / 5;
                 var bufferLat = (maxLat - minLat) / 5;
-
                 var minLat = minLat - bufferLat,
                     minLng = minLng - bufferLng,
                     maxLat = maxLat + bufferLat,
                     maxLng = maxLng + bufferLng;
-
-
+                    */
                 var bbox = {
                     minLat: minLat,
                     minLng: minLng,
                     maxLat: maxLat,
                     maxLng: maxLng
                 };
+                /*
+                 if (minLat == maxLat) {
+                 minLat -= 0.1;
+                 maxLat += 0.1;
+                 }
+                 if (minLng == maxLng) {
+                 minLng -= 0.1;
+                 maxLng += 0.1;
+                 }
+                 */
+                /*
+                 if (maxLng - minLng < maxLat - minLat) {
+                 while (maxLng - minLng < maxLat - minLat) {
+                 maxLng = maxLng + 0.01;
+                 minLng = minLng - 0.01;
+                 }
+                 } else {
+                 while (maxLng - minLng > maxLat - minLat) {
+                 minLat = minLat - 0.01;
+                 maxLat = maxLat + 0.01;
+                 }
+                 }
+                 */
 
-                surfaceImage2.sector.minLatitude=minLat;
-                surfaceImage2.sector.minLongitude=minLng;
-                surfaceImage2.sector.maxLatitude=maxLat;
-                surfaceImage2.sector.maxLongitude=maxLng;
+               // var bbox = wwd.navigator.getVisibleAreaBoundaries();
 
-                var bbox = {
-                    minLat: minLat,
-                    minLng: minLng,
-                    maxLat: maxLat,
-                    maxLng: maxLng
-                };
+                console.log(bbox);
+                surfaceImage2.sector.minLatitude = minLat;
+                surfaceImage2.sector.minLongitude = minLng;
+                surfaceImage2.sector.maxLatitude = maxLat;
+                surfaceImage2.sector.maxLongitude = maxLng;
+
                 var t = Transformation;
                 t.setPoints([[0, canvas.height], [0, 0], [canvas.width, 0], [canvas.width, canvas.height]],
                     [[bbox.minLng, bbox.minLat], [bbox.minLng, bbox.maxLat], [bbox.maxLng, bbox.maxLat], [bbox.maxLng, bbox.minLat]]);
                 heat.clear();
                 points.forEach(function (p) {
-                    var out = t.transform(p);
-                    var x = out[0];
-                    var y = canvas.height - out[1];
-                    heat.add([x, y, 100]);
+                    if (bbox.minLat <= p[1] && p[1] <= bbox.maxLat && bbox.minLng <= p[0] && p[0] <= bbox.maxLng) {
+                        var out = t.transform(p);
+                        var x = out[0];
+                        var y = out[1];//canvas.height -
+                        heat.add([x, y, 100]);
+                    } else {
+                        console.log("point outside");
+                    }
+
+
                 });
-                heat.resize();
+                heat.add([0, 0, 100]);
+                heat.add([canvas.height, canvas.height, 100]);
+                heat.add([0, canvas.height, 100]);
+                heat.add([canvas.height, 0, 100]);
+               // heat.resize();
+                heat.draw(0);
+                surfaceImage2.imageSourceWasUpdated = true;
             } catch (e) {
                 console.log(e);
             }
-*/
 
 
-            if (range > 4000000) {
-                radius = 95;
-            } else {
-                var addition = 20;
-                if (range < bufferLng * 100000) {
-                    addition = 10 - ((10000 / range )/ (bufferLng * 10));
-                    heat._fixedOpacity = 100 + addition * 10;
-                } else {
-                    heat._fixedOpacity = null;
-                }
-                var radius = Math.abs((range / 20000) + addition) / (bufferLng * 5);
-            }
-            console.log(radius);
-            heat.radius(radius, radius / 2)
-            heat.draw(0);
-            surfaceImage2.imageSourceWasUpdated = true;
-            wwd.redraw();
+            /*
+             if (range > 4000000) {
+             radius = 95;
+             } else {
+             var addition = 20;
+             if (range < bufferLng * 100000) {
+             addition = 10 - ((10000 / range )/ (bufferLng * 10));
+             heat._fixedOpacity = 100 + addition * 10;
+             } else {
+             heat._fixedOpacity = null;
+             }
+             var radius = Math.abs((range / 20000) + addition) / (bufferLng * 5);
+             }
+             console.log(radius);
+             heat.radius(radius, radius / 2)
+             heat.draw(0);
+             surfaceImage2.imageSourceWasUpdated = true;
+             wwd.redraw();
+             */
         }
 
         var surfaceImage2 = new WorldWind.SurfaceImage(new WorldWind.Sector(bbox.minLat, bbox.maxLat, bbox.minLng, bbox.maxLng),
@@ -212,9 +244,54 @@ requirejs(['../src/WorldWind',
         var layerManger = new LayerManager(wwd);
 
         var navigator = wwd.navigator;
+
+
+        navigator.bbox = function () {
+
+            this.applyLimits();
+            var globe = this.worldWindow.globe,
+                lookAtPosition = new WorldWind.Position(this.lookAtLocation.latitude, this.lookAtLocation.longitude, 0),
+                modelviewMatrix = WorldWind.Matrix.fromIdentity();
+            modelviewMatrix.multiplyByLookAtModelview(lookAtPosition, this.range, this.heading, this.tilt, this.roll, globe);
+
+
+            var globe = this.worldWindow.globe,
+                globeRadius = WorldWind.WWMath.max(globe.equatorialRadius, globe.polarRadius),
+                eyePoint = modelviewMatrix.extractEyePoint(new WorldWind.Vec3(0, 0, 0)),
+                eyePos = globe.computePositionFromPoint(eyePoint[0], eyePoint[1], eyePoint[2], new WorldWind.Position(0, 0, 0));
+            return eyePos;
+        };
+
+        navigator.getVisibleAreaBoundaries = function () {
+
+            navigator.bbox();
+            var w = this.worldWindow.canvas.width;
+            var h = this.worldWindow.canvas.height;
+            var center = wwd.pickTerrain(new WorldWind.Vec2(500, 500));
+            if (center.objects[0].position) {
+                center = center.objects[0].position;
+
+
+                var l = this.range / Math.cos(Math.PI / 8);
+                var base = Math.sqrt(Math.pow(l, 2) - Math.pow(this.range, 2));
+
+                base = base / 100000;
+                minLat = center.latitude - base;
+                maxLat = center.latitude + base;
+                minLng = center.longitude - base;
+                maxLng = center.longitude + base;
+                var bbox = {
+                    minLat: minLat,
+                    minLng: minLng,
+                    maxLat: maxLat,
+                    maxLng: maxLng
+                };
+                return bbox
+            }
+
+        };
+
         navigator.handleWheelEvent = function (event) {
-            // Normalize the wheel delta based on the wheel delta mode. This produces a roughly consistent delta across
-            // browsers and input devices.
             var normalizedDelta;
             if (event.deltaMode == WheelEvent.DOM_DELTA_PIXEL) {
                 normalizedDelta = event.deltaY;
@@ -223,16 +300,13 @@ requirejs(['../src/WorldWind',
             } else if (event.deltaMode == WheelEvent.DOM_DELTA_PAGE) {
                 normalizedDelta = event.deltaY * 400;
             }
-
-            // Compute a zoom scale factor by adding a fraction of the normalized delta to 1. When multiplied by the
-            // navigator's range, this has the effect of zooming out or zooming in depending on whether the delta is
-            // positive or negative, respectfully.
             var scale = 1 + (normalizedDelta / 1000);
 
             // Apply the scale to this navigator's properties.
             this.range *= scale;
             this.applyLimits();
             this.worldWindow.redraw();
+            //var visibleArea = this.getVisibleAreaBoundaries();
 
             drawHeatmap(this.range);
         }
